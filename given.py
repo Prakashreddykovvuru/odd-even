41b04f22f5d14df7ed4b94000e4cda2ca82b9c3d
Prakashreddykovvuru <76939491+Prakashreddykovvuru@users.noreply.github.com>
2021-01-04 13:52:35 +0530

    Add files via upload

---
+++ given.py
@@ -0,0 +1,12 @@
+# How to know given number is even or odd:
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
+num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+   # print(num,"is Even")
+# else:
+   # print(num,"is Odd")
+
+print("Even") if (num % 2) == 0 else print(num,"add")
